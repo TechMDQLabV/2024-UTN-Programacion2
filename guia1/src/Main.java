4b7ab03e5f78efb54a5ddfdfc7ab491d78009eb1
Sergio Garguir <sergiogarguir@gmail.com>
2024-08-20 21:39:46 -0300

    add Personas y builder

--- guia1/src/Main.java
+++ guia1/src/Main.java
@@ -1,28 +1,36 @@
 import models.Persona;
+import models.Personas;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    private static Scanner menuScanner = new Scanner(System.in);
+    private static final Scanner menuScanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int opcion;
-        Persona o = new Persona("Juan", "Molina");
-        o.setAge(56);
-        Persona p = new Persona("Pepe");
-        System.out.println(p.getName() + " " + p.getLastName() + " " + p.getAge());
-        System.out.println(o.getName() + " " + o.getLastName() + " " + o.getAge());
-        p.setName("Moni");
-        p.setLastName("Argento");
-        System.out.println(p.getName() + " " + p.getLastName());
-        System.out.println(o);
+        int option;
+        Persona p = new Persona()
+                .name("Pepe")
+                .lastName("Argento")
+                .age(56);
 
-        do{
+        p.imprimir();
+
+        Personas personas = new Personas(100);
+        personas.cargar(p);
+        personas.cargar(new Persona()
+                .name("Juana")
+                .lastName("Molina")
+                .age(45));
+        personas.cargar(new Persona().name("Anibal"));
+        personas.cargar(new Persona().lastName("Ramirez").age(57));
+        personas.mostrar();
+
+        do {
             menu();
-            opcion = menuScanner.nextInt();
+            option = menuScanner.nextInt();
 
-            switch(opcion){
+            switch (option) {
                 case 1:
                     ej01();
                     break;
@@ -36,29 +44,22 @@ public class Main {
                 case 5:
                     break;
             }
-
-        }while(opcion != 0);
+        } while (option != 0);
         menuScanner.close();
     }
 
-    private static void menu(){
-        System.out.println("\n<<<<<<<<<<<< Menu de Opciones >>>>>>>>>>>>>\n");
-        System.out.println("1 - Ejercicio 1");
-        System.out.println("2 - Ejercicio 2");
-        System.out.println("3 - Ejercicio 3");
-        System.out.println("4 - Ejercicio 4");
-        System.out.println("5 - Ejercicio 5");
+    private static void menu() {
+        System.out.println("\n\t\t\t<<<<<<<<<<<< Menu de Opciones >>>>>>>>>>>>>\n");
+        System.out.println("1 - Ej 1");
+        System.out.println("2 - Ej 2");
+        System.out.println("3 - Ej 3");
+        System.out.println("4 - Ej 4");
+        System.out.println("5 - Ej 5");
         System.out.println(" ");
         System.out.println("0 para Salir");
     }
-/* Realizar un programa Java que realice lo siguiente: declarar una variable
-numN de tipo int, una variable numA de tipo double y una variable numC de
-tipo char y asigna a cada una un valor. A continuación muestra por pantalla:
-El valor de cada variable, la suma de numN + numA, la diferencia de numA -
-numN, el valor numérico correspondiente al carácter que contiene la
-variable numC.
- */
-    private static void ej01(){
+
+    private static void ej01() {
         int numI = 666;
         double numD = 2.33;
         char numC = 'c';
@@ -67,19 +68,16 @@ variable numC.
         System.out.printf("el nroI es: %d - el nroD es: %.2f - el numC es: %d ", numI, numD, numChar);
     }
 
-    private static void ejString(){
+    private static void ejString() {
         String name = "Pepe";
         int[] array = new int[20];
         array[5] = 666;
         char[] c = new char[10];
+
         Arrays.sort(array);
         System.out.println(Arrays.toString(array));
-        for(int i : array){
+        for (int i : array) {
             System.out.println(i);
         }
-       /* for(int i=0;i<array.length;i++){
-            System.out.println(array[i]);
-        }*/
     }
-
 }
